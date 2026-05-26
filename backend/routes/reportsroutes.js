9bba4aca5a281e1146c958b89cb329bc605d10a5
AabirRoy2007 <aabirroy30@gmail.com>
2026-05-26 12:22:13 +0530

    Keep only frontend

--- backend/routes/reportsroutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const ctrl   = require('../controllers/reports.controller');
-const { verifyToken, verifyAdmin } = require('../middleware/auth.middleware');
-
-router.get('/',    verifyToken, ctrl.getAll);
-router.post('/',   verifyAdmin, ctrl.generate);
-
-module.exports = router;
